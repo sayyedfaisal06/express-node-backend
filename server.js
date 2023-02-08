882a06e827aff0d5f14d2c4c181ec5e037b7ff71
Faisal Sayyed <sayyedfaisal06@gmail.com>
2023-02-09 01:08:35 +0530

    consistant code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
 
