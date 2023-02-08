9a71d13bf52450c105e55db89103e4d1b61223f1
Faisal Sayyed <sayyedfaisal06@gmail.com>
2023-02-09 01:13:21 +0530

    added date-fns and uuid

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
